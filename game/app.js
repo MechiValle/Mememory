e6ab0fffbdd7bf1eaecf03fb055368cfbd16448c
MechiValle <mercedesvalle17@gmail.com>
2021-12-29 22:16:28 -0300

    Setting de score con la fórmula tiempo restante - movimientos efectuados en la partida

--- game/app.js
+++ game/app.js
@@ -1,25 +1,99 @@
 //Creamos un bool que de true cuando la carta está dada vuelta
 let flippedCard = false;
-
 //Creamos variables que contengan la primera y la segunda carta.
 let card1, card2;
 
 //Creamos el bool lockBoard para impedir que el usuario de vuelta más de dos cartas a la vez
 let lockBoard = false;
 
+const cardsData = {
+    backFace: { class: "back", src: "images/backface.png" },
+    frontFace: {
+        class: "front",
+        imagesSrc: [
+            "images/meme1.png",
+            "images/meme2.png",
+            "images/meme3.png",
+            "images/meme4.png",
+            "images/meme5.png",
+            "images/meme6.png",
+        ],
+    },
+};
+
+// Creamos una funcion que va a recibir por parametro los datos que vienen del Endpoint
+function createCard(imgFront, imgBack, classFront, classBack, dataId) {
+    // creamos un div con el metodo createElement() (Manipulacion del DOM)
+    const container = document.createElement("div");
+// creamos un template literal (bloque de HTML en JS) que contiene placeholders
+    // que nos van a permitir recibir los argumentos de la funcion
+    const card = `
+    <div class="card" data-match="${dataId}">
+        <img class="${classFront}" src="${imgFront}" alt="" />
+        <img class="${classBack}" src="${imgBack}" alt="" />
+    </div>`;
+    // inyectamos la informacion card (template literal) dentro del div que creamos anteriormente
+    container.innerHTML = card;
+
+    return container;
+}
+
+// creamos una funcion para crear el board
+function createBoard(cards) {
+    // creamos un section con el metodo createElement() (Manipulacion del DOM)
+    const grid = document.createElement("section");
+    // agregamos a la section a la clase grid
+    grid.classList.add("grid");
+    // agregamos a la section un ID tambien llamado grid
+    grid.setAttribute("id", "grid");    
+// guardaoms la data que viene del Endpoint en variables para poder utilizarlas luego en las funciones Create Card
+const imgsFront = cards.frontFace.imagesSrc;
+const imgBack = cards.backFace.src;
+const classFront = cards.frontFace.class;
+const classBack = cards.backFace.class;
+
+// guardamos en una variable el elemento HTML con ID board
+const board = document.getElementById("board");
+
+// Recorremos el array de imagenes que se encuentra en nuestro Endpoint y en cada iteracion ejecutamos la funcion createCard() que nos devuelve el doble de cards.
+imgsFront.forEach((imgFront) => {
+    const cardElement = createCard(
+        imgFront,
+        imgBack,
+        classFront,
+        classBack,
+        imgFront
+    );
+    const cardElement2 = createCard(
+        imgFront,
+        imgBack,
+        classFront,
+        classBack,
+        imgFront
+    );
+    // Con el metodo appendChild insertamos las cards (HTML) en el grid
+    grid.appendChild(cardElement);
+    grid.appendChild(cardElement2);
+});
+// Con el metodo appendChild insertamos el grid en el board (unico elemento HTML que creamos en index.html)
+board.appendChild(grid);
+}
+
+createBoard(cardsData);
+
 //Seleccionar todas las cartas con su clase para después usarlas en las funciones
 const cards = document.querySelectorAll(".card");
 
+
 //Por cada carta, cuando el usuario clickea, se va a llamar a la funcion flipCard
 cards.forEach((card) => card.addEventListener("click", flipCard));
-
 function flipCard() {
     //Chequeamos que esta función se ejecute solo cuando this está asignado a la primera carta, para evitar que se pueda matchear haciendo dos clicks sobre una misma carta
     if (this === card1) return;
-
     if (lockBoard) return;
     //Agregamos la clase 'flip' a la carta una vez que se clickea
     this.classList.add("flip");
+    movesCounter()
     if (!flippedCard) {
         //Pasamos el bool a true
         flippedCard = true;
@@ -32,7 +106,6 @@ function flipCard() {
         matchTwo();
     }
 }
-
 function matchTwo() {
     //Vamos a chequear si las dos coinciden (usando el atributo data)
     if (card1.dataset.match === card2.dataset.match) {
@@ -42,9 +115,21 @@ function matchTwo() {
     }
 }
 
+
 // Creamos un contador de tiempo
-let count = 0;
+let count = 200;
+let moves = 0;
+
+const scoreCounter = document.getElementById("score");
+const scoreTitle = document.getElementById("scoreTitle");
 const counterElement = document.getElementById("time");
+const movesCount = document.getElementById("moves");
+
+function movesCounter(){
+    movesCount.innerText ++;
+    moves ++;
+}
+
 // Creamos una variable que donde se encuentra el estado del juego,
 // y nos va a permitir verificar si el juego termino.
 let gameFinished = false;
@@ -52,9 +137,14 @@ let gameFinished = false;
 // Creamos una funcion setInterval que se ejecuta cada un segundo mientras el juego no haya terminado,
 // suma 1 al contador de tiempo y lo muestra en el html.
 let finalCount = setInterval(() => {
-    if (!gameFinished) {
-        count++;
+    if (!gameFinished && count >= 1) {
+        count--;
         counterElement.innerText = count;
+    }
+    else{
+        clearInterval(finalCount);
+        lockBoard = true;
+        document.getElementById("replay").classList.add("show");
     }
 }, 1000);
 
@@ -70,6 +160,9 @@ function disableCards() {
     // Agregamos un boton que solo sera visible cuando el juego haya finalizado
     if (gameFinished) {
         document.getElementById("replay").classList.add("show");
+        scoreTitle.classList.add("show");
+        scoreCounter.classList.add("show");
+        scoreCounter.innerText = count - moves;
     }
     reset();
 }
